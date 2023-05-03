4d2675b27b02f14db4f2266898a47122c9cd8a17
loretari <loretari@gmail.com>
2023-05-03 11:03:21 +0300

    init: add Error page with errorElement

---
+++ src/pages/Error.js
@@ -0,0 +1,16 @@
+import ManiNavigation from "../components/MainNavigation";
+
+function ErrorPage() {
+    return(
+        <>
+            <ManiNavigation/>
+            <main>
+                <h1>An error occurred!</h1>
+                <p>Could not find this page!</p>
+            </main>
+
+            </>
+    )
+}
+
+export default ErrorPage;
--- src/pages/Roots.js
+++ src/pages/Roots.js
@@ -1,12 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import ManiNavigation from "../components/MainNavigation";
-import classes from './Root.module.css'
+
 
 function RootLayout () {
 return (
     <>
   <ManiNavigation/>
-        <main className={classes.content}>
+        <main>
             <Outlet />
         </main>
 
