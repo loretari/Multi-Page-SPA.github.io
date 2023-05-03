05e0402f4150eebb6c5c82d4f33f6a47e62c132f
loretari <loretari@gmail.com>
2023-05-03 14:21:11 +0300

    init: defining & using Dynamic Routes

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HomePage from "./pages/Home";
 import ProductsPage from "./pages/Products";
 import RootLayout from "./pages/Roots";
 import ErrorPage from "./pages/Error";
+import ProductDetailPage from "./pages/ProductDetail";
 
 // const routeDefinitions = createRoutesFromElements(
 //     <Route>
@@ -18,7 +19,8 @@ const router = createBrowserRouter([
         children: [
 
             {path: '/', element: <HomePage/>},
-            {path: '/products', element: <ProductsPage/>}
+            {path: '/products', element: <ProductsPage/>},
+            {path: "/products/:productId", element: <ProductDetailPage/>}
         ]
     },
 
